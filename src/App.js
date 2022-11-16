7a758bf040a513ff64314dd9a4535578eb3d0cd0
DmitriH08 <113308027+DmitriH08@users.noreply.github.com>
2022-11-16 16:46:20 +0200

    deafault search

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // API KEY : 2d5debd860354c8fa67d4458c2ad9578
+// API KEY at home : 38fb7c09c1fb4382ab54dbc123733022
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import NewsGroupComponent from './Body';
--- src/Services/apiServices.js
+++ src/Services/apiServices.js
@@ -1,3 +1,3 @@
 export async function getEverything(data){
-    return await fetch ('https://newsapi.org/v2/everything?q=bitcoin&apiKey=2d5debd860354c8fa67d4458c2ad9578')
+    return await fetch ('https://newsapi.org/v2/everything?q=bitcoin&apiKey='+ process.env.REACT_APP_API_KEY);
 }
